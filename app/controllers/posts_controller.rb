ef2bb95cae406ebd9a39d48b87c84264d3a984ec
Anderson <andersonpirespereira@hotmail.com>
2017-01-06 21:18:05 -0600

    Add view files and edit / destroy functions to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,62 @@
 class PostsController < ApplicationController
+	# calls the method find_post before the show, edit, update and destroy methods
+	# DRY Principle
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	
+	# Creates the index method
 	def index
+	end
+
+	# Shows the post
+	def show	
+	end
+
+	# Creates the new post view
+	def new
+		@post = Post.new
+	end
+
+	# Creates the new post
+	def create
+		# Gets the parameters for the new post
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	# Creates the edit view
+	def edit
+	end
+
+	# Updates the post
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	# Deletes the post
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	# Methods bellow are private
+	private
+
+	# Finds a specific post
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	# Gets the title and content of the post (DRY Principle)
+	def post_params
+		params.require(:post).permit(:title, :content)
 	end
 end
